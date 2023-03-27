0571e7b14e7fbbfe18a28c1ab5528188590afe32
MartinxMax <280901844@qq.com>
2023-03-27 22:07:16 +0800

    v1.0.0

---
+++ MCollider.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+# @Мартин.
+import sys,argparse,textwrap,hashlib
+from loguru import logger
+from multiprocessing.dummy import Pool as ThreadPool
+
+Version = "@Мартин. MCollider_MD5 Tool V1.0.0"
+Title='''
+************************************************************************************
+<免责声明>:本工具仅供学习实验使用,请勿用于非法用途,否则自行承担相应的法律责任
+<Disclaimer>:This tool is onl y for learning and experiment. Do not use it for illegal purposes, or you will bear corresponding legal responsibilities
+************************************************************************************'''
+Logo=f'''
+   ▄▄▄▄███▄▄▄▄         ▄████████       ▄██████▄        ▄█             ▄█             ▄█       ████████▄          ▄████████         ▄████████ 
+ ▄██▀▀▀███▀▀▀██▄      ███    ███      ███    ███      ███            ███            ███       ███   ▀███        ███    ███        ███    ███ 
+ ███   ███   ███      ███    █▀       ███    ███      ███            ███            ███▌      ███    ███        ███    █▀         ███    ███ 
+ ███   ███   ███      ███             ███    ███      ███            ███            ███▌      ███    ███       ▄███▄▄▄           ▄███▄▄▄▄██▀ 
+ ███   ███   ███      ███             ███    ███      ███            ███            ███▌      ███    ███      ▀▀███▀▀▀          ▀▀███▀▀▀▀▀   
+ ███   ███   ███      ███    █▄       ███    ███      ███            ███            ███       ███    ███        ███    █▄       ▀███████████ 
+ ███   ███   ███      ███    ███      ███    ███      ███▌    ▄      ███▌    ▄      ███       ███   ▄███        ███    ███        ███    ███ 
+  ▀█   ███   █▀       ████████▀        ▀██████▀       █████▄▄██      █████▄▄██      █▀        ████████▀         ██████████        ███    ███ 
+                                                      ▀              ▀                                                            ███    ███ 
+                                                    Github==>https://github.com/MartinxMax    
+                                                    {Version}  
+'''
+
+
+
+def Init_Loger():
+    logger.remove()
+    logger.add(
+        sink=sys.stdout,
+        format="<green>[{time:HH:mm:ss}]</green><level>[{level}]</level> -> <level>{message}</level>",
+        level="INFO"
+    )
+
+
+class Main_Class():
+    def __init__(self,args):
+        self.MD5=args.MD5
+        self.CODE_LIST = []
+
+    def run(self):
+        if self.MD5:
+            logger.warning("Cracking containing MD5 values of:"+self.MD5)
+            pool = ThreadPool()
+            for i in range(10):
+                self.CODE_LIST.append( str(10000000 * i)+ ':' + str(10000000 * (i + 1)))
+            logger.info("Cracking....")
+            pool.map(self.Crack_MD5, self.CODE_LIST)
+            pool.close()
+            pool.join()
+
+
+    def Get_MD5(self,note):
+        return hashlib.md5(str(note).encode('utf-8')).hexdigest()
+
+
+    def Crack_MD5(self,scope):
+        start_num=int(scope.split(':')[0])
+        end_num=int(scope.split(':')[-1])
+        logger.info(f"[Runing] Decoding range {start_num}-{end_num} pure digits")
+        for i in range(start_num, end_num):
+            if self.MD5 in self.Get_MD5(i):
+                logger.warning(f"{i} is contained by MD5:{self.Get_MD5(i)}")
+                break
+
+
+def main():
+    print(Logo,"\n",Title)
+    Init_Loger()
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog=textwrap.dedent('''
+        Example:
+            author-Github==>https://github.com/MartinxMax
+        Basic usage:
+            python3 {MPHP} -md5 xxxxxxxx # Support for fuzzy blasting
+            '''.format(MPHP = sys.argv[0]
+                )))
+    parser.add_argument('-md5', '--MD5',default=None, help='MD5')
+    args = parser.parse_args()
+    Main_Class(args).run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,42 @@
+  <div align="center">
+<p align="center">
+ <img title="MCollider" src='https://img.shields.io/badge/MCollider-1.0.0-brightgreen.svg' />
+ <img title="MCollider" src='https://img.shields.io/badge/Number_MD5_Crack-Tool'/>
+ <img title="MCollider" src='https://img.shields.io/badge/Python-3.9-yellow.svg' />
+  <img title="MCollider" src='https://img.shields.io/badge/HackerTool-x' />
+ <img title="MCollider" src='https://img.shields.io/static/v1?label=Author&message=@Martin&color=red'/>
+ <img title="MCollider" src='https://img.shields.io/badge/-Linux-F16061?logo=linux&logoColor=000'/>
+ </p>
+  <img height="137px" src="https://github-readme-stats.vercel.app/api?username=MartinXMax&hide_title=true&hide_border=true&show_icons=trueline_height=21&text_color=000&icon_color=000&bg_color=0,ea6161,ffc64d,fffc4d,52fa5a&theme=graywhite" />
+  
+   
+ <table>
+  <tr>
+      <th>Function</th>
+  </tr>
+  <tr>
+    <th>Digital Blasting MD5</th>
+  </tr>
+ 
+ </table>
+</div>
+
+## usage method
+  * View help information
+
+      ```#python3 MCollider.py -h```
+
+  ![图片名称](./PT/help.png)  
+
+# MD5 Blast
+
+ ```#python3 MPHP.py -md5 a13213```
+
+
+  ![图片名称](./PT/run.png)  
+
+
+
+  ![图片名称](./PT/flag.png)  
+  
+ 
